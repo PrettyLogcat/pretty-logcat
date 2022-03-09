a68d7492b5a01cbb000b0a3ab05aa12bc1926b22
DonizeteVida <donizetevida1998@outlook.com>
2022-03-09 17:14:24 -0300

    Testing some documentation approachs

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,34 @@
 use regex::Regex;
 
+use std::ops::Deref;
+
 use crate::data::{Data, DataBuilder};
 
 pub struct Parser(Regex);
 
+impl Deref for Parser {
+    type Target = Regex;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 pub struct ParserData<'a>(Vec<&'a str>);
+
+impl<'a> Deref for ParserData<'a> {
+    type Target = Vec<&'a str>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'a> From<ParserData<'a>> for Vec<&'a str> {
+    fn from(parser_data: ParserData<'a>) -> Self {
+        parser_data.0
+    }
+}
 
 impl Parser {
     pub fn new(regex: &str) -> Parser {
@@ -12,18 +36,16 @@ impl Parser {
     }
 
     pub fn parse<'a>(&self, data: &'a str) -> Option<ParserData<'a>> {
-        match self.0.captures(&data) {
+        match self.captures(&data) {
             Some(capture) => {
-                let mut data = Vec::new();
+                let mut vec = Vec::new();
                 let mut sub_capture_matches = capture.iter();
                 //Skip first capture because it's always full string
                 sub_capture_matches.next();
-                while let Some(option_match) = sub_capture_matches.next() {
-                    if let Some(some_match) = option_match {
-                        data.push(some_match.as_str())
-                    }
+                while let Some(Some(some_match)) = sub_capture_matches.next() {
+                    vec.push(some_match.as_str());
                 }
-                Some(ParserData(data))
+                Some(ParserData(vec))
             }
             None => None,
         }
@@ -32,6 +54,7 @@ impl Parser {
 
 impl<'a> DataBuilder<'a> for ParserData<'a> {
     fn build(self) -> Data<'a> {
-        Data(self.0)
+        //I dont know why From its not working
+        Data(From::from(self))
     }
 }
